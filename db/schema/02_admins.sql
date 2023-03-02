ebe7fa8b71f43ccee879ae57fadcc3504f8dfa48
DASitby <davidsooley551@gmail.com>
2023-03-02 22:50:31 +0000

    create admins table DDL from ERD

---
+++ db/schema/02_admins.sql
@@ -0,0 +1,9 @@
+-- Drop and recreate Users table (Example)
+
+DROP TABLE IF EXISTS admins CASCADE;
+CREATE TABLE admins (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(15)
+);
