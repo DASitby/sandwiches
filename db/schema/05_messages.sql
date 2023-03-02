22642e6834a3000202629e35ce14898f9e9336fb
DASitby <davidsooley551@gmail.com>
2023-03-02 23:59:56 +0000

    apply consistent use for user/admin as opposed to buyer/seller in sales, listings and messages

--- db/schema/05_messages.sql
+++ db/schema/05_messages.sql
@@ -4,6 +4,6 @@ DROP TABLE IF EXISTS messages CASCADE;
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
   sale_id INTEGER REFERENCES sales(id) ON DELETE CASCADE,
-  seller_is_sender BOOLEAN NOT NULL,
+  admin_is_sender BOOLEAN NOT NULL,
   body TEXT NOT NULL
 );
