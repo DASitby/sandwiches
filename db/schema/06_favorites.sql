00f028dad85f5de28d5e661c5a16f101837423c4
DASitby <davidsooley551@gmail.com>
2023-03-02 23:37:17 +0000

    create favorites table DDL from ERD

---
+++ db/schema/06_favorites.sql
@@ -0,0 +1,8 @@
+-- Drop and recreate Messages table (Example)
+
+DROP TABLE IF EXISTS favorites CASCADE;
+CREATE TABLE favorites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  listing_id INTEGER REFERENCES listings(id) ON DELETE CASCADE
+);
