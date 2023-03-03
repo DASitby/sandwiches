050a36a54dde555887dc8777904c5c9fd298bb45
DASitby <davidsooley551@gmail.com>
2023-03-03 12:14:15 +0000

    change erroneous password property name to phone_number

--- db/seeds/02_admins.sql
+++ db/seeds/02_admins.sql
@@ -1,201 +1,201 @@
 -- Admins table seeds here
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'James McGuire', 'victoriaellis@hotmail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Lucille Holt', 'allisonvalencia@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Eugene Klein', 'lilaayers@ymail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Hallie Spencer', 'carolinebradley@icloud.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Nelle Little', 'aubreyrichmond@aol.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Corey Dean', 'noahnolan@live.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Donald Wright', 'gabriellahouse@hotmail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Thomas Wright', 'annabellekoch@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Phoebe Brewer', 'evelynbuck@aol.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Luke Hicks', 'masoncline@hotmail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Sue Doyle', 'katherinegraves@aol.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Augusta Moore', 'clairecompton@ymail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Beatrice Thomas', 'cadencewilliamson@inbox.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Allen Johnson', 'zoewood@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Rebecca Anderson', 'stellapeterson@icloud.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Kate Boyd', 'lilareyes@google.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Amanda Cortez', 'kaitlynshepherd@aol.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Mary Santiago', 'henrygilliam@yahoo.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Eleanor Todd', 'aubreyholloway@live.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Jesus Pierce', 'graysonbarrett@ymail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Lula Cannon', 'calliedillard@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Adele Green', 'noraheath@gmail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Derrick Sanders', 'kayladelaney@hotmail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Owen Adams', 'nolanrodriguez@yahoo.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Lenora Quinn', 'wyattnash@icloud.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Eunice McDaniel', 'declanelliott@hotmail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Alice Hughes', 'lukewooten@live.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Aiden Santos', 'isabellemclaughlin@inbox.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Jim Garner', 'elenaodom@google.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Florence Ryan', 'nolandonaldson@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Emilie Hodges', 'miamccarthy@gmail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Georgia Holt', 'rydercantu@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Christina Ingram', 'levireed@icloud.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Jerome Cannon', 'allisonhinton@google.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Julian Zimmerman', 'jamesbritt@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Jimmy Boyd', 'tylerberg@ymail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Ray Padilla', 'gabriellaporter@outlook.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Johnny Nguyen', 'amandafrye@icloud.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Anne Goodwin', 'isabellehead@google.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Matthew Banks', 'hannahyates@inbox.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Gene Burns', 'danielsargent@outlook.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Nicholas Drake', 'matthewnorman@yahoo.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Andre Sims', 'samantharamos@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Rose Patrick', 'cadenbanks@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Juan Brady', 'averyhurley@gmail.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Irene Hale', 'sadiemcneil@icloud.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Duane Nichols', 'sebastiantrujillo@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Lulu Gonzalez', 'carsonpugh@live.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Sallie Dunn', 'josephvelasquez@gmx.com', 1234567890);
     INSERT INTO admins (
-    name, email, password)
+    name, email, phone_number)
     VALUES (
     'Jerome Freeman', 'rileymay@gmail.com', 1234567890);
