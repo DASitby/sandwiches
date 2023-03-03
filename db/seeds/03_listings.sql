52555f0f4a507a0c6b09bbae4e8b83ec37e7c6e6
DASitby <davidsooley551@gmail.com>
2023-03-03 01:35:00 +0000

    36 listing seeds created

---
+++ db/seeds/03_listings.sql
@@ -0,0 +1,145 @@
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (1, '2023-02-26', 'Classic Reuben', 'A classic sandwich, delicious if you are into that sort of thing', false, false, 'M', 'sliced', 'beef', 799);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (2, '2023-02-27', 'Superior Reuben', 'A way better version of the classic sandwich, delicious and you will  be way more into it than our the version our competitor makes', false, false, 'M', 'sliced', 'beef', 999);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (3, '2023-03-01', 'Gluten-Free Reuben', 'A classic sandwich, delicious if you are into that sort of thing, made gluten free for your needs!', true, false, 'M', 'sliced', 'beef', 1199);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (3, '2023-03-01', 'Vegetarian Reuben', 'A classic sandwich, delicious if you are into that sort of thing made vegetarian for your needs!', false, true, 'M', 'sliced', 'tofu', 899);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (3, '2023-03-02', 'Vegetarian Gluten-Free Reuben', 'A classic sandwich, delicious if you are into that sort of thing, now available for both your dietary restrictions!', true, true, 'M', 'sliced', 'tofu', 1399);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (1, '2023-02-26', 'Large Reuben', 'A classic sandwich, delicious if you are into that sort of thing', false, false, 'L', 'sliced', 'beef', 799);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (2, '2023-02-27', 'Large Superior Reuben', 'A way better version of the classic sandwich, delicious and you will  be way more into it than our the version our competitor makes', false, false, 'L', 'sliced', 'beef', 999);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (3, '2023-03-01', ' Large Gluten-Free Reuben', 'A classic sandwich, delicious if you are into that sort of thing, made gluten free for your needs!', true, false, 'L', 'sliced', 'beef', 1199);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (3, '2023-03-01', 'Large Vegetarian Reuben', 'A classic sandwich, delicious if you are into that sort of thing made vegetarian for your needs!', false, true, 'L', 'sliced', 'tofu', 899);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (3, '2023-03-02', 'Large Vegetarian Gluten-Free Reuben', 'A classic sandwich, delicious if you are into that sort of thing, now available for both your dietary restrictions!', true, true, 'L', 'sliced', 'tofu', 1399);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (1, '2023-02-26', 'Small Reuben', 'A classic sandwich, delicious if you are into that sort of thing', false, false, 'S', 'sliced', 'beef', 799);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (2, '2023-02-27', 'Small Superior Reuben', 'A way better version of the classic sandwich, delicious and you will  be way more into it than our the version our competitor makes', false, false, 'S', 'sliced', 'beef', 999);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (3, '2023-03-01', 'Small Gluten-Free Reuben', 'A classic sandwich, delicious if you are into that sort of thing, made gluten free for your needs!', true, false, 'S', 'sliced', 'beef', 1199);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (3, '2023-03-01', 'Small Vegetarian Reuben', 'A classic sandwich, delicious if you are into that sort of thing made vegetarian for your needs!', false, true, 'S', 'sliced', 'tofu', 899);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (3, '2023-03-02', 'Small Vegetarian Gluten-Free Reuben', 'A classic sandwich, delicious if you are into that sort of thing, now available for both your dietary restrictions!', true, true, 'S', 'sliced', 'tofu', 1399);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (4, '2023-02-26', 'Classic Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing', false, false, 'M', 'sliced', 'chicken', 799);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (5, '2023-02-27', 'Superior Chicken Parm', 'A way better version of the classic sandwich, delicious and you will  be way more into it than our the version our competitor makes', false, false, 'M', 'sliced', 'chicken', 999);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (6, '2023-03-01', 'Gluten-Free Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing, made gluten free for your needs!', true, false, 'M', 'sliced', 'chicken', 1199);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (6, '2023-03-01', 'Vegetarian Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing made vegetarian for your needs!', false, true, 'M', 'sliced', 'tofu', 899);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (6, '2023-03-02', 'Vegetarian Gluten-Free Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing, now available for both your dietary restrictions!', true, true, 'M', 'sliced', 'tofu', 1399);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (4, '2023-02-26', 'Large Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing', false, false, 'L', 'sliced', 'chicken', 799);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (5, '2023-02-27', 'Large Superior Chicken Parm', 'A way better version of the classic sandwich, delicious and you will  be way more into it than our the version our competitor makes', false, false, 'L', 'sliced', 'chicken', 999);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (6, '2023-03-01', ' Large Gluten-Free Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing, made gluten free for your needs!', true, false, 'L', 'sliced', 'chicken', 1199);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (6, '2023-03-01', 'Large Vegetarian Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing made vegetarian for your needs!', false, true, 'L', 'sliced', 'tofu', 899);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (6, '2023-03-02', 'Large Vegetarian Gluten-Free Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing, now available for both your dietary restrictions!', true, true, 'L', 'sliced', 'tofu', 1399);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (4, '2023-02-26', 'Small Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing', false, false, 'S', 'sliced', 'chicken', 799);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (5, '2023-02-27', 'Small Superior Chicken Parm', 'A way better version of the classic sandwich, delicious and you will  be way more into it than our the version our competitor makes', false, false, 'S', 'sliced', 'chicken', 999);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (6, '2023-03-01', 'Small Gluten-Free Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing, made gluten free for your needs!', true, false, 'S', 'sliced', 'chicken', 1199);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (6, '2023-03-01', 'Small Vegetarian Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing made vegetarian for your needs!', false, true, 'S', 'sliced', 'tofu', 899);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (6, '2023-03-02', 'Small Vegetarian Gluten-Free Chicken Parm', 'A classic sandwich, delicious if you are into that sort of thing, now available for both your dietary restrictions!', true, true, 'S', 'sliced', 'tofu', 1399);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (7, '2023-03-02', 'Small Meatball Sub', 'I cook the meatball ok', false, false, 'S', 'sub', 'meatball', 1099);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (7, '2023-03-02', 'Meatball Sub', 'I cook the meatball ok', false, false, 'M', 'sub', 'meatball', 1399);
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price
+)
+VALUES (7, '2023-03-02', 'Large Meatball Sub', 'I cook the meatball ok', false, false, 'L', 'sub', 'meatball', 1699);
+
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price, sold, sold_date
+)
+VALUES (1, '2023-02-23', 'Classic Reuben', 'A classic sandwich, delicious if you are into that sort of thing', false, false, 'M', 'sliced', 'beef', 799, true, '2023-02-23');
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price, sold, sold_date
+)
+VALUES (1, '2023-02-24', 'Classic Reuben', 'A classic sandwich, delicious if you are into that sort of thing', false, false, 'M', 'sliced', 'beef', 799, true, '2023-02-24');
+INSERT INTO listings (
+ admin_id, start_date, title, description, gluten_free, vegetarian, size, bread_type, protein, price, sold, sold_date
+)
+VALUES (1, '2023-02-25', 'Classic Reuben', 'A classic sandwich, delicious if you are into that sort of thing', false, false, 'M', 'sliced', 'beef', 799, true, '2023-02-25');
