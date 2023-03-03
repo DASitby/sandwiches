b14f3e10f8a14f98663cfdb051839db45620b4c4
DASitby <davidsooley551@gmail.com>
2023-03-03 00:31:20 +0000

    50 user seeds created

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,201 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds here
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Devin Sanders', 'tristanjacobs@gmail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Iva Harrison', 'allisonjackson@mail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Lloyd Jefferson', 'asherpoole@gmx.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Dale Coleman', 'michaelgray@mail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Alejandro Osborne', 'ariaatkinson@outlook.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Nell Medina', 'juliansantos@aol.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Estelle Walsh', 'elistanton@yahoo.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Herbert Graves', 'emilyowen@live.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'John Stevens', 'charliebattle@yahoo.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Isabelle Robbins', 'miasutton@aol.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Jerome Wright', 'bellaanthony@gmail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'May Barrett', 'gabriellabarrera@aol.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Christine Buchanan', 'nathannguyen@ymail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Oscar Little', 'alainamcfarland@hotmail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Mabelle Hughes', 'jordansears@outlook.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Adelaide Harrington', 'ariawolfe@outlook.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Ola Brock', 'alainajames@gmail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Gavin Cook', 'nolanbriggs@gmail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Harriett Alvarez', 'kaelynross@gmail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Effie Simpson', 'muhammadwebb@hotmail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Mamie Webster', 'ianhale@ymail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Jennie Powers', 'lilabell@live.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Maggie Norris', 'miafletcher@inbox.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Justin Reyes', 'austindotson@aol.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Effie Phillips', 'evelynaustin@ymail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Travis Mendoza', 'lucypittman@gmx.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Edgar Harrison', 'keiragates@ymail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Delia Caldwell', 'elliotbaker@yahoo.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Calvin Thornton', 'rileyrichard@outlook.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Elizabeth Lane', 'henryhaynes@gmx.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'James Banks', 'sydneylopez@live.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Micheal Page', 'kennedyparks@mail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Owen Powell', 'ariagallagher@gmail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Rosetta Walsh', 'kylierhodes@mail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Louis Washington', 'chloecarter@google.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Pauline Cruz', 'hannahpickett@inbox.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Patrick Hammond', 'cameroncherry@yahoo.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Nora Alexander', 'coltonlove@aol.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Maria Nelson', 'alexandravalencia@ymail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Frances Lowe', 'mackenziebutler@mail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Johanna Powell', 'lilymorgan@mail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Jay Welch', 'avagilliam@icloud.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Rose Cummings', 'gabriellabarr@mail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Mattie Larson', 'baileyforeman@inbox.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Olive Romero', 'buwalls@live.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Don Perkins', 'alexandrahancock@yahoo.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Madge Gray', 'violetmcintyre@inbox.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Susan Scott', 'adrianbyrd@gmx.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Sue Daniel', 'nataliefletcher@gmail.com', 1234567890);
+    INSERT INTO users (
+    name, email, phone_number)
+    VALUES (
+    'Beatrice Sanders', 'zacharybullock@inbox.com', 1234567890);
