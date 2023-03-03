586327c61be7f2b6051a0ddece5aa16d989cfbdd
DASitby <davidsooley551@gmail.com>
2023-03-03 00:43:15 +0000

    50 admin seeds created

---
+++ db/seeds/02_admins.sql
@@ -0,0 +1,201 @@
+-- Admins table seeds here
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'James McGuire', 'victoriaellis@hotmail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Lucille Holt', 'allisonvalencia@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Eugene Klein', 'lilaayers@ymail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Hallie Spencer', 'carolinebradley@icloud.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Nelle Little', 'aubreyrichmond@aol.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Corey Dean', 'noahnolan@live.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Donald Wright', 'gabriellahouse@hotmail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Thomas Wright', 'annabellekoch@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Phoebe Brewer', 'evelynbuck@aol.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Luke Hicks', 'masoncline@hotmail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Sue Doyle', 'katherinegraves@aol.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Augusta Moore', 'clairecompton@ymail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Beatrice Thomas', 'cadencewilliamson@inbox.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Allen Johnson', 'zoewood@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Rebecca Anderson', 'stellapeterson@icloud.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Kate Boyd', 'lilareyes@google.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Amanda Cortez', 'kaitlynshepherd@aol.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Mary Santiago', 'henrygilliam@yahoo.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Eleanor Todd', 'aubreyholloway@live.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Jesus Pierce', 'graysonbarrett@ymail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Lula Cannon', 'calliedillard@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Adele Green', 'noraheath@gmail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Derrick Sanders', 'kayladelaney@hotmail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Owen Adams', 'nolanrodriguez@yahoo.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Lenora Quinn', 'wyattnash@icloud.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Eunice McDaniel', 'declanelliott@hotmail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Alice Hughes', 'lukewooten@live.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Aiden Santos', 'isabellemclaughlin@inbox.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Jim Garner', 'elenaodom@google.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Florence Ryan', 'nolandonaldson@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Emilie Hodges', 'miamccarthy@gmail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Georgia Holt', 'rydercantu@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Christina Ingram', 'levireed@icloud.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Jerome Cannon', 'allisonhinton@google.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Julian Zimmerman', 'jamesbritt@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Jimmy Boyd', 'tylerberg@ymail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Ray Padilla', 'gabriellaporter@outlook.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Johnny Nguyen', 'amandafrye@icloud.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Anne Goodwin', 'isabellehead@google.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Matthew Banks', 'hannahyates@inbox.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Gene Burns', 'danielsargent@outlook.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Nicholas Drake', 'matthewnorman@yahoo.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Andre Sims', 'samantharamos@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Rose Patrick', 'cadenbanks@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Juan Brady', 'averyhurley@gmail.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Irene Hale', 'sadiemcneil@icloud.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Duane Nichols', 'sebastiantrujillo@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Lulu Gonzalez', 'carsonpugh@live.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Sallie Dunn', 'josephvelasquez@gmx.com', 1234567890);
+    INSERT INTO admins (
+    name, email, password)
+    VALUES (
+    'Jerome Freeman', 'rileymay@gmail.com', 12345678902a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
