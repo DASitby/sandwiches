b02eef6d22ae58d0ef16eea4737672efbc96555c
DASitby <davidsooley551@gmail.com>
2023-03-03 01:58:35 +0000

    add 37 sales seeds

---
+++ db/seeds/04_sales.sql
@@ -0,0 +1,148 @@
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (1, 1, 1);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (1, 3, 1);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (1, 5, 1);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (1, 7, 1);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (1, 9, 1);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (1, 11, 1);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (1, 13, 1);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (1, 15, 1);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (1, 17, 1);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (2, 2, 2);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (2, 4, 2);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (2, 6, 2);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (3, 1, 3);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 21, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 22, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 23, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 24, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 25, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 26, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 27, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 28, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 29, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 30, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 31, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 32, 4);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (16, 37, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 38, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 39, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 40, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 41, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 42, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 43, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 44, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 45, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 46, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 47, 7);
+INSERT INTO sales (
+  listing_id, user_id, admin_id
+)
+VALUES (31, 48, 7);
