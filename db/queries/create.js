eca0257dda2a5ee01ceb99ac859319b619268574
Ozzy Tasci <oguzcantasci@gmail.com>
2023-03-05 13:30:08 -0500

    Fix syntax error

--- db/queries/create.js
+++ db/queries/create.js
@@ -14,7 +14,7 @@ const createListing = (listing) => {
       protein,
       price,
       thumbnail_url)
-     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
+     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
      RETURNING *;`,
     [listing.admin_id,
       Date.now(),
