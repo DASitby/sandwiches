c0197323be362cb77704eb613cda5e68a2980552
Ozzy Tasci <oguzcantasci@gmail.com>
2023-03-05 10:36:34 -0500

    Fix logic for cookie setting

--- public/scripts/welcome.js
+++ public/scripts/welcome.js
@@ -1,11 +1,14 @@
 // Client facing scripts here
+
 $(() => {
   $('#user-btn').on('click', (event) => {
+    document.cookie = "admin= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
     event.preventDefault();
     document.cookie = "user=true";
     window.location.href = '/listings';
   });
   $('#admin-btn').on('click', (event) => {
+    document.cookie = "user= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
     event.preventDefault();
     document.cookie = "admin=true";
     window.location.href = '/listings';
