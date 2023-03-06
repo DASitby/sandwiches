008191824affe9f59e4e1530ab35619b65d12fed
DASitby <davidsooley551@gmail.com>
2023-03-06 13:32:23 +0000

    add seeds for user 1 favorites to test feature

---
+++ db/seeds/05_favorites.sql
@@ -0,0 +1,20 @@
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 1);
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 3);
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 5);
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 7);
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 9);
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 11);
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 13);
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 15);
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 17);
+INSERT INTO favorites (user_id, listing_id)
+VALUES (1, 19);
