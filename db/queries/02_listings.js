9ab0458736809ee9f027f27dfe10e01b5d927c9b
DASitby <davidsooley551@gmail.com>
2023-03-06 01:57:49 +0000

    add query construction for the size, gluten, and vegetarian properties

--- db/queries/02_listings.js
+++ db/queries/02_listings.js
@@ -1,12 +1,41 @@
 const db = require('../connection');
 
-const getListings = () => {
-  return db.query(
-    `SELECT listings.id, title, admins.name as seller, thumbnail_url
-    FROM listings
-    JOIN admins on admins.id = admin_id
-    ORDER BY RANDOM()
-    LIMIT 9;`)
+
+const getListings = (options = {}) => {
+  const queryParams = [];
+  let queryString = `SELECT listings.id, title, admins.name as seller, thumbnail_url
+  FROM listings
+  JOIN admins on admins.id = admin_id
+  ORDER BY RANDOM()`;
+
+  const checkWhere = (string) => {
+    if (string.includes('WHERE')) {
+      queryString += 'WHERE';
+    } else {
+      queryString += 'and';
+    }
+  };
+
+  if (options.size) {
+    queryParams.push(`${options.size}`);
+    checkWhere(queryString);
+    queryString += `size = $${queryParams.length}`;
+  }
+
+  if (options.gluten_free) {
+    queryParams.push(`${options.gluten_free}`);
+    checkWhere(queryString);
+    queryString += `gluten_free = $${queryParams.length}`;
+  }
+
+  if (options.vegetarian) {
+    queryParams.push(`${options.vegetarian}`);
+    checkWhere(queryString);
+    queryString += `vegetarian = $${queryParams.length}`;
+  }
+
+  queryString += `LIMIT 9`;
+  return db.query(queryString, queryParams)
     .then(data => {
       return data.rows;
     })
