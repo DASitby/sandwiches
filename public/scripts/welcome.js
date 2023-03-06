d86055835f7c6249c6497b791be584fe75b79100
DASitby <davidsooley551@gmail.com>
2023-03-06 13:44:06 +0000

    add admin_id and user_id to cookie for myListings and Favorites testing

--- public/scripts/welcome.js
+++ public/scripts/welcome.js
@@ -3,14 +3,18 @@
 $(() => {
   $('#user-btn').on('click', (event) => {
     document.cookie = "admin= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = "admin_id= ";
     event.preventDefault();
     document.cookie = "user=true";
+    document.cookie = "user_id=1";
     window.location.href = '/listings';
   });
   $('#admin-btn').on('click', (event) => {
     document.cookie = "user= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = "user_id= ";
     event.preventDefault();
     document.cookie = "admin=true";
+    document.cookie = "admin_id=1";
     window.location.href = '/listings';
   });
 });
