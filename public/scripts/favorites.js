af320aab47aca28888a32f287f9718f95d1ce90e
DASitby <davidsooley551@gmail.com>
2023-03-07 22:41:02 +0000

    fix typo

--- public/scripts/favorites.js
+++ public/scripts/favorites.js
@@ -1,6 +1,6 @@
 $(() => {
   $('.favorite-btn').on('click', (event) => {
     let listingID = event.target.baseURI.substring(31);
-    $.post(`/favorites/:${listingID}`);
+    $.post(`/favorites/${listingID}`);
   });
 });
