88851b0694dea147ccd9fffd2f21482610b0bf4c
Ozzy Tasci <oguzcantasci@gmail.com>
2023-03-06 19:36:49 -0500

    Add route to add listing to favorites

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const router  = express.Router();
-const { getListings } = require('../db/queries/02_listings');
+const { createFavorite } = require('../db/queries/05_createFavorite.js');
+const { getListings } = require('../db/queries/02_listings.js');
 
 
 router.get('/', (req, res) => {
   //Parse cookie for user_id
   let user_id;
   let cookieArray = req.headers.cookie.split(" ");
-  console.log(cookieArray);
   for (let i = 0; i < cookieArray.length; i++) {
     if (cookieArray[i].includes("user_id")) {
       let userArray = cookieArray[i].split('=');
       user_id = Number(userArray[1]);
     }
   }
-  console.log(user_id);
   const options = {
     user_id: user_id
   };
@@ -23,5 +22,20 @@ router.get('/', (req, res) => {
       const templateVars = { listings: listings, cookie: req.headers.cookie};
       res.render('index', templateVars);
     });
+});
+
+router.post('/', (req, res) => {
+  let path = req.headers.referer;
+  let listingID = Number(path.substring(31));
+  let user_id;
+  let cookieArray = req.headers.cookie.split(" ");
+  for (let i = 0; i < cookieArray.length; i++) {
+    if (cookieArray[i].includes("user_id")) {
+      let userArray = cookieArray[i].split('=');
+      user_id = Number(userArray[1]);
+    }
+  }
+  createFavorite(user_id, listingID);
+  res.redirect('back');
 });
 module.exports = router;
