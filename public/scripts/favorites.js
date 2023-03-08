09da23abb396295dd95365b44b03dd98eaedff2e
DASitby <davidsooley551@gmail.com>
2023-03-08 23:24:51 +0000

    adjust if statement to read new favorites property and change class on filled in star to trigger different jquery

--- public/scripts/favorites.js
+++ public/scripts/favorites.js
@@ -6,7 +6,7 @@ $(() => {
 });
 
 $(() => {
-  $('.favorite-btn').on('click', (event) => {
+  $('.del-favorite-btn').on('click', (event) => {
     let listingID = event.target.baseURI.substring(31);
     $.post(`/favorites/delete/${listingID}`);
   });
