b3e507347922dd4b73602e4038f1a71c0cede8da
DASitby <davidsooley551@gmail.com>
2023-03-09 01:00:59 +0000

    add some seeds for a sample conversation

---
+++ db/seeds/06_messages.sql
@@ -0,0 +1,23 @@
+-- Classic Reuben Conversation
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (1, false, "Hello I would Like this sandwich but it's too small?");
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (1, true, "I have listings for larger sandwiches available");
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (1, false, "How do I find them?");
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (1, true, "The website has helpful search features in the top right!");
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (1, false, "Thanks!");
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (1, true, "No problem! :)");
+-- Large Reuben Conversation
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (2, false, "Can I have this one?");
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (2, true, "Yes!");
+-- Small Reuben Conversation
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (3, false, "Too small!!!!");
+INSERT INTO messages (sale_id, admin_is_sender, body)
+VALUES (3, true, "Try another sandwich, I have a large size!");
