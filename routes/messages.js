ba5def66b0fbd32e86d28f274789a0a9df59113f
Ozzy Tasci <oguzcantasci@gmail.com>
2023-03-09 11:53:14 -0500

    Pass the data from database to the front-end for rendering

--- routes/messages.js
+++ routes/messages.js
@@ -7,10 +7,38 @@
 
 const express = require('express');
 const router  = express.Router();
+const { getMessagesAdmin, getMessagesUser } = require('../db/queries/08_messages');
 
 router.get('/', (req, res) => {
-  const templateVars = {cookie: req.headers.cookie};
-  res.render('messages', templateVars);
+  let id;
+  let cookieArray = req.headers.cookie.split(" ");
+  cookieArray.forEach(cookie => {
+    const [key, value] = cookie.split('=');
+    if (key === 'user_id') {
+      const numValue = Number(value.substring(0,1)); // Convert the value to a number
+      if (!isNaN(numValue)) { // Check if the value is a number
+        console.log(`${key} has a number value: ${numValue}`);
+        id = numValue;
+        getMessagesUser(id)
+          .then((messages) => {
+            const templateVars = {messages, cookie: req.headers.cookie};
+            res.render('messages', templateVars);
+          });
+      }
+    }
+    if (key === 'admin_id') {
+      const numValue = Number(value.substring(0,1)); // Convert the value to a number
+      if (!isNaN(numValue)) { // Check if the value is a number
+        console.log(`${key} has a number value: ${numValue}`);
+        id = numValue;
+        getMessagesAdmin(id)
+          .then((messages) => {
+            const templateVars = {messages, cookie: req.headers.cookie};
+            res.render('messages', templateVars);
+          });
+      }
+    }
+  });
 });
 
 module.exports = router;
