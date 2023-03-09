81fa7d775fba5fc0fbdc28368c8c0300816317ef
Ozzy Tasci <oguzcantasci@gmail.com>
2023-03-09 17:33:42 -0500

    Add logic to display listing information of the message

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
       method: 'GET',
       success: function(messages) {
         // Handle the response from the server
-
+        let gluten_free;
+        let vegetarian;
         for (let message of messages) {
           if (message.sale_id === saleId) {
             if (message.admin_is_sender) {
@@ -20,8 +21,41 @@ $(document).ready(function() {
               $('.messages-conversation-container').append(`<div class="user-conversation-container">ADMIN: ${message.body}</div>`);
             }
           }
-
         }
+
+        for (let message of messages) {
+          if (message.sale_id === saleId) {
+            if (message.glute_free) {
+              gluten_free = 'Yes';
+            } else {
+              gluten_free = 'No';
+            }
+            if (message.vegetarian) {
+              vegetarian = 'Yes';
+            } else {
+              vegetarian = 'No';
+            }
+            $('.listing-display-container').html(`
+            <div class="messages-image-container">
+            <img src="${message.thumbnail_url}" alt="listing image">
+            </div>
+
+          <section class="listing-description-container">
+            <ul>
+              <td><span style="color: #e0e0e0">TITLE: ${message.title}</span></td><br>
+              <td><span style="color: #e0e0e0">DESCRIPTION: ${message.description}</span></td><br>
+              <td><span style="color: #e0e0e0">SIZE: ${message.size} </span></td><br>
+              <td><span style="color: #e0e0e0">GLUTEN FREE: ${gluten_free}</span></td><br>
+              <td><span style="color: #e0e0e0">VEGETARIAN: ${vegetarian}</span></td><br>
+              <td><span style="color: #e0e0e0">PROTEIN: ${message.protein}</span></td><br>
+              <td><span style="color: #e0e0e0">BREAD: ${message.bread_type}</span></td><br>
+              <td><span style="color: #e0e0e0">PRICE: ${message.price}</span></td>
+            </ul>
+          </section>
+            `);
+          }
+        }
+
       },
       error: function(xhr, status, error) {
         // Handle any errors that occur during the AJAX request
