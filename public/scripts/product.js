4d7b1d3098cd71dc8d981a974576176bcdc947ae
DASitby <davidsooley551@gmail.com>
2023-03-10 01:44:58 +0000

    add script to fire buy handler

--- public/scripts/product.js
+++ public/scripts/product.js
@@ -18,3 +18,10 @@ $(() => {
     $.post(`/listings/${listingID}/delete`);
   });
 });
+
+$(() =>{
+  $('.buy-btn').on('click', (event) => {
+    let listingID = event.target.baseURI.substring(31);
+    $.post(`/listings/${listingID}/buy`);
+  });
+});
