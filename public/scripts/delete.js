0873ec3a0fe117e3aeeaedc522a35a5fb39a87be
DASitby <davidsooley551@gmail.com>
2023-03-10 01:24:36 +0000

    merge favorites and delete script files into product script file

--- public/scripts/delete.js
+++
@@ -1,6 +0,0 @@
-$(() => {
-  $('.garbage-icon').on('click', (event) => {
-    let listingID = event.target.baseURI.substring(31);
-    $.post(`/listings/${listingID}/delete`);
-  });
-});
--- public/scripts/product.js
+++ public/scripts/product.js
@@ -11,3 +11,10 @@ $(() => {
     $.post(`/favorites/delete/${listingID}`);
   });
 });
+
+$(() => {
+  $('.garbage-icon').on('click', (event) => {
+    let listingID = event.target.baseURI.substring(31);
+    $.post(`/listings/${listingID}/delete`);
+  });
+});
