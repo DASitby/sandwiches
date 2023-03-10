479d938f746acae9932257ef7ad3aed53ae9589b
DASitby <davidsooley551@gmail.com>
2023-03-10 01:44:22 +0000

    add a /buy post handler to create sales entities

--- routes/product.js
+++ routes/product.js
@@ -9,6 +9,8 @@ const express = require('express');
 const router  = express.Router();
 const { getSandwich } = require('../db/queries/03_product');
 const { deleteListing } = require('../db/queries/06_deleteListing.js');
+const { createMessage } = require('../db/queries/08_messages');
+const { createSale } = require('../db/queries/09_createSale');
 
 router.get('/:id', (req, res) => {
   let sandwichID = req.params.id;
@@ -41,6 +43,24 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/:id/buy', (req, res) => {
+  let sandwichID = req.params.id;
+  let user_id;
+  let cookieArray = req.headers.cookie.split(" ");
+  cookieArray.forEach(cookie => {
+    const [key, value] = cookie.split('=');
+    const numValue = Number(value.substring(0,1)); // Convert the value to a number
+    if (!isNaN(numValue)) { // Check if the value is a number
+      console.log(`${key} has a number value: ${numValue}`);
+      user_id = numValue;
+    }
+  });
+  createSale(sandwichID,user_id)
+    .then((sale) => {
+      createMessage(sale.id,false,'Hello, I would like to negotiate purchase of this sandwich!');
+      res.redirect('/messages/');
+    });
+});
 
 router.post('/:id/delete', (req, res) => {
   let sandwichID = req.params.id;
